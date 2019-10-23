ddc1de5d7cce28eb9245a9650dd30a0debde9ccd
Alex Aquino <alexaquino.it@gmail.com>
2019-10-23 14:23:19 -0300

    adiciona modularização

--- imc.py
+++ imc.py
@@ -1,103 +1,83 @@
+"""
 # -*- coding: utf-8 -*-
 
-"""
-Esse módulo implementa a tarefa 001 ('indice de massa corporal (IMC) da disciplina Fundamentos de Programação e 
-Engenharia de Software - PES-001(2019.2) do Programa de Pós-Graduação em Neuroengenharia do Instituto Internacional 
-de Neurociências Edmond e Lily Safra (IIN-ELS).
+Esse módulo implementa os métodos necessários para calcular o indice de massa corporal (IMC) do 
+indivíduo. O IMC pode ajudar a identificar obesidade ou desnutrição em crianças, adolescentes, 
+adultos e idosos.
 
-Requisitos e orientações para resolução dessa tarefa encontram-se na wiki do projeto: 
+Requisitos e orientações para resolução desse problema encontram-se na wiki do projeto: 
 _Wiki: https://github.com/alexaquino/IINELS-PES-001/wiki.
 
 :Authors: Alex Aquino, Bruno Spinelli, Pablo Queiroz, Rodrigo Henrique
-:Version: 1.0.0
+:Version: 2.1.0
 :License: MIT
 """
 
-
-# Variáveis Globais 
-pesoQuilos = 70.00
-alturaMetros = 1.80
-
-
 # Métodos 
 
-def calcularIndiceMassaCorporal(pesoQuilos, alturaMetros):
-    """calcularIndiceMassaCorporal realiza o cálculo do índice de massa corporal (IMC)
-        :param pesoQuilos: peso do indivíduo em quilos
-        :type pesoQuilos: float
-        :param alturaMetros: altura do indivíduo metros
-        :type alturaMetros: float
-        :return indiceMassaCorporal: índice de massa corporal (IMC)
+def calcular_indice_massa_corporal(__peso_quilos, __altura_metros):
+    """Realiza o cálculo do índice de massa corporal (IMC)
+        :param __peso_quilos: peso do indivíduo em quilos
+        :type __peso_quilos: float
+        :param __altura_metros: altura do indivíduo metros
+        :type __altura_metros: float
+        :return __indice_massa_corporal: índice de massa corporal (IMC)
         :rtype: float
     """
-    indiceMassaCorporal = (pesoQuilos / (alturaMetros ** 2)) 
-    isMuitoAbaixoPeso(indiceMassaCorporal)
-    isAbaixoPeso(indiceMassaCorporal)
-    isPesoNormal(indiceMassaCorporal)
-    isAcimaPeso(indiceMassaCorporal)
-    isMuitoAcimaPeso(indiceMassaCorporal)
-    return indiceMassaCorporal
+    __indice_massa_corporal = (__peso_quilos / (__altura_metros ** 2)) 
+    return __indice_massa_corporal
 
 
-def isMuitoAbaixoPeso(indiceMassaCorporal):
-    """isMuitoAbaixoPeso verifica se o indivíduo está muito abaixo do peso ideal
-        :param indiceMassaCorporal: indice de massa corporal do indivíduo
-        :type indiceMassaCorporal: float
-        :return isMuitoAbaixoPeso: resultado da verificação (indiceMassaCorporal < 17)
+def is_muito_abaixo_peso(__indice_massa_corporal):
+    """Verifica se o indivíduo está muito abaixo do peso ideal
+        :param __indice_massa_corporal: indice de massa corporal do indivíduo
+        :type __indice_massa_corporal: float
+        :return __is_muito_abaixo_peso: True ou False para IMC < 17
         :rtype: bool
     """
-    isMuitoAbaixoPeso = (indiceMassaCorporal < 17)
-    print("\n - Está muito abaixo do peso ideal? ", isMuitoAbaixoPeso)
-    return isMuitoAbaixoPeso
+    __is_muito_abaixo_peso = (__indice_massa_corporal < 17)
+    return __is_muito_abaixo_peso
 
 
-def isAbaixoPeso(indiceMassaCorporal):
-    """isAbaixoPeso verifica se o indivíduo está abaixo do peso ideal
-        :param indiceMassaCorporal: indice de massa corporal do indivíduo
-        :type indiceMassaCorporal: float
-        :return isAbaixoPeso: resultado da verificação (indiceMassaCorporal >= 17 and indiceMassaCorporal <= 18.5)
+def is_abaixo_peso(__indice_massa_corporal):
+    """Verifica se o indivíduo está abaixo do peso ideal
+        :param __indice_massa_corporal: indice de massa corporal do indivíduo
+        :type __indice_massa_corporal: float
+        :return __is_abaixo_peso: True ou False para IMC >= 17 e IMC <= 18.5
         :rtype: bool
     """
-    isAbaixoPeso = (indiceMassaCorporal >= 17 and indiceMassaCorporal <= 18.5)
-    print("\n - Está abaixo do peso ideal? ", isAbaixoPeso)
-    return isAbaixoPeso
+    __is_abaixo_peso = (__indice_massa_corporal >= 17 and __indice_massa_corporal < 18.5)
+    return __is_abaixo_peso
 
 
-def isPesoNormal(indiceMassaCorporal):
-    """isPesoNormal verifica se o indivíduo está com o peso ideal
-        :param indiceMassaCorporal: indice de massa corporal do indivíduo
-        :type indiceMassaCorporal: float
-        :return isPesoNormal: resultado da verificação (indiceMassaCorporal >= 18.5 and indiceMassaCorporal indiceMassaCorporal <= 25)
+def is_peso_normal(__indice_massa_corporal):
+    """Verifica se o indivíduo está com o peso ideal
+        :param __indice_massa_corporal: indice de massa corporal do indivíduo
+        :type __indice_massa_corporal: float
+        :return __is_peso_normal: True ou False para IMC >= 18.5 e IMC <= 25
         :rtype: bool
     """
-    isPesoNormal = (indiceMassaCorporal >= 18.5 and indiceMassaCorporal <= 25)
-    print("\n - Está com o peso ideal? ", isPesoNormal)
-    return isPesoNormal
+    __is_peso_normal = (__indice_massa_corporal >= 18.5 and __indice_massa_corporal < 25)
+    return __is_peso_normal
   
   
-def isAcimaPeso(indiceMassaCorporal):
-    """isAcimaPeso verifica se o indivíduo está acima do peso ideal
+def is_acima_peso(__indice_massa_corporal):
+    """Verifica se o indivíduo está acima do peso ideal
         :param indiceMassaCorporal: indice de massa corporal do indivíduo
         :type indiceMassaCorporal: float
-        :return isAcimaPeso: resultado da verificação (indiceMassaCorporal >= 25 and indiceMassaCorporal indiceMassaCorporal <= 30)
+        :return is_acima_peso: True ou False para IMC >= 25 e IMC <= 30
         :rtype: bool
     """
-    isAcimaPeso = (indiceMassaCorporal >= 25 and indiceMassaCorporal <= 30)
-    print("\n - Está acima do peso ideal? ", isAcimaPeso)
-    return isAcimaPeso
+    __is_acima_peso = (__indice_massa_corporal >= 25 and __indice_massa_corporal < 30)
+    return __is_acima_peso
 
 
-def isMuitoAcimaPeso(indiceMassaCorporal):
-    """isMuitoAcimaPeso verifica se o indivíduo está muito acima do peso ideal
-        :param indiceMassaCorporal: indice de massa corporal do indivíduo
-        :type indiceMassaCorporal: float
-        :return isMuitoAcimaPeso: resultado da verificação (indiceMassaCorporal > 30)
+def is_muito_acima_peso(__indice_massa_corporal):
+    """Verifica se o indivíduo está muito acima do peso ideal
+        :param __indice_massa_corporal: indice de massa corporal do indivíduo
+        :type __indice_massa_corporal: float
+        :return is_muito_acima_peso: True ou False para IMC > 30
         :rtype: bool
     """
-    isMuitoAcimaPeso = (indiceMassaCorporal > 30)
-    print("\n - Está muito acima do peso ideal? ", isMuitoAcimaPeso)
-    return isMuitoAcimaPeso
-
-
-# Chamada de Métodos
-calcularIndiceMassaCorporal(pesoQuilos, alturaMetros)
+    __is_muito_acima_peso = (__indice_massa_corporal >= 30)
+    return __is_muito_acima_peso
